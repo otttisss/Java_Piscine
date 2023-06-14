d44bc049ea1f1ff59a3163a171e6b641bab1985a
otttisss <otttisss@yandex.ru>
2023-06-14 15:29:48 +0300

    ex03 fixes added

--- day01/ex03/TransactionsLinkedList.java
+++ day01/ex03/TransactionsLinkedList.java
@@ -18,6 +18,11 @@ public class TransactionsLinkedList implements TransactionsList {
             this.t = t;
         }
     }
+
+    public int getSize() {
+        return size;
+    }
+
     @Override
     public void addTransaction(Transaction transaction) {
         final Node tmp = last;
@@ -37,10 +42,47 @@ public class TransactionsLinkedList implements TransactionsList {
         if (id == null) {
             throw new TransactionNotFoundException();
         }
+        for (Node tmp = first; tmp != null; tmp = tmp.next) {
+            if (tmp.t != null && id.equals(tmp.t.getIdentifier())) {
+                deleteNodeFromLinkedList(tmp);
+                return;
+            }
+        }
+        throw new TransactionNotFoundException();
     }
 
     @Override
     public Transaction[] toArray() {
-        return new Transaction[0];
+        if (this.size == 0) {
+            return null;
+        }
+        Transaction[] result = new Transaction[this.size];
+
+        int i = 0;
+        for (Node tmp = first; tmp != null; tmp = tmp.next) {
+            result[i++] = tmp.t;
+        }
+        return result;
+    }
+
+    private void deleteNodeFromLinkedList(Node node) {
+        final Node next = node.next;
+        final Node prev = node.prev;
+
+        if (prev == null)
+            this.first = next;
+        else {
+            prev.next = next;
+            node.prev = null;
+        }
+        if (next == null)
+            this.last = prev;
+        else {
+            next.prev = prev;
+            node.next = null;
+        }
+
+        node.t = null;
+        size--;
     }
 }
