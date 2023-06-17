dbe013335ef4c2dd035e2eecd1dc99db9a3cfa8c
otttisss <otttisss@yandex.ru>
2023-06-17 14:33:51 +0300

    day04 ex02 currently in progress

---
+++ day04/ex02/ImagesToChar/src/java/edu/school21/printer/logic/Logic.java
@@ -0,0 +1,28 @@
+package edu.school21.printer.logic;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Logic {
+
+    public static void imageToChar(char white, char black) {
+        try {
+            BufferedImage image = ImageIO.read(Logic.class.getResource("/resources/it.bmp"));
+
+            for (int y = 0; y < image.getHeight(); y++) {
+                for (int x = 0; x < image.getWidth(); x++) {
+                    if (image.getRGB(x, y) == Color.BLACK.getRGB())
+                        System.out.print(black);
+                    else if (image.getRGB(x, y) == Color.WHITE.getRGB())
+                        System.out.print(white);
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
