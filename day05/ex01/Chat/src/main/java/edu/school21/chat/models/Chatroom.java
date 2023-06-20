989922c62f8d4abdadba8e97cf8a264326be8337
otttisss <otttisss@yandex.ru>
2023-06-20 18:25:03 +0300

    day05 ex01 in progress

--- day05/ex01/Chat/src/main/java/edu/school21/chat/models/Chatroom.java
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/models/Chatroom.java
@@ -1,4 +1,87 @@
 package edu.school21.chat.models;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Chatroom {
+    private int id;
+    private String room_name;
+    private User owner;
+    private List<Message> messageList;
+
+    public Chatroom(int id, String room_name, User owner, List<Message> messageList) {
+        this.id = id;
+        this.room_name = room_name;
+        this.owner = owner;
+        this.messageList = messageList;
+    }
+
+    public Chatroom(int id, String room_name) {
+        this.id = id;
+        this.room_name = room_name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, room_name, owner, messageList);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Chatroom chatroom = (Chatroom) obj;
+        if (id != chatroom.id)
+            return false;
+        if (owner != chatroom.owner)
+            return false;
+        if (!Objects.equals(room_name, chatroom.room_name))
+            return false;
+        return Objects.equals(messageList, chatroom.messageList);
+    }
+
+    @Override
+    public String toString() {
+        return "Chatroom{" +
+                "id = " + id +
+                ", name = " + room_name + '\'' +
+                ", owner = " + owner +
+                ", messageList = " + messageList +
+                "}";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRoom_name() {
+        return room_name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public List<Message> getMessageList() {
+        return messageList;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRoom_name(String room_name) {
+        this.room_name = room_name;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setMessageList(List<Message> messageList) {
+        this.messageList = messageList;
+    }
 }
--- day05/ex01/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepositoryJdbcImpl.java
+++ day05/ex01/Chat/src/main/java/edu/school21/chat/repositories/MessagesRepositoryJdbcImpl.java
@@ -1,4 +1,37 @@
 package edu.school21.chat.repositories;
+import edu.school21.chat.models.*;
 
-public class MessagesRepositoryJdbcImpl {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+import javax.sql.DataSource;
+
+public class MessagesRepositoryJdbcImpl implements MessagesRepository {
+    private final DataSource dataSource;
+    private final String query = "select * from chat.messages where id = ";
+
+    public MessagesRepositoryJdbcImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Optional<Message> findById(Long id) throws SQLException {
+        try {
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query + id);
+
+            if (resultSet.next()) {
+                return Optional.of(new Message(resultSet.getInt("id"),
+                        new User(resultSet.getInt("author"), resultSet.getString(2), resultSet.getString(3)),
+                        new Chatroom(resultSet.getInt("room_name"), resultSet.getString(2)),
+                        resultSet.getString("message"), resultSet.getTimestamp("time").toLocalDateTime()));
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return Optional.empty();
+    }
 }
