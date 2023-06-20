832b1f5dbb96903c843d95d7073f01579d0697e6
otttisss <otttisss@yandex.ru>
2023-06-20 15:24:32 +0300

    day05 ex00 maven project added

--- day05/ex00/Chat/src/main/resources/schema.sql
+++ day05/ex00/Chat/src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 create schema if not exists chat;
 
-create table if not exists chat_users (
+create table if not exists chat.users (
     id          serial primary key,
-    login       varchar(50) not null,
-    password    varchar(50) not null
+    login       text not null,
+    password    text not null
 );
 
-create table if not exists chat_rooms (
+create table if not exists chat.rooms (
     id          serial primary key,
     room_name   text not null,
     owner       integer references chat_users(id) not null
 );
 
-create table if not exists chat_messages (
+create table if not exists chat.messages (
     id          serial primary key,
     author      integer references chat_users(id) not null,
     room        integer references chat_rooms(id) not null,
