49388db5f3a5093291da3e3a8ce61595cb4c0cb2
otttisss <otttisss@yandex.ru>
2023-06-22 20:40:34 +0300

    day06 ex01 completed?

--- day06/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSource.java
+++ day06/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSource.java
@@ -1,4 +1,24 @@
 package edu.school21.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.junit.jupiter.api.Assertions;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 public class EmbeddedDataSource {
+    private DataSource dataSource;
+
+    @BeforeEach
+    private void init() {
+        dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL)
+                .addScript("schema.sql").addScript("data.sql").build();
+    }
+
+    @Test
+    public void getConnection() throws SQLException {
+        Assertions.assertNotNull(dataSource.getConnection());
+    }
 }
