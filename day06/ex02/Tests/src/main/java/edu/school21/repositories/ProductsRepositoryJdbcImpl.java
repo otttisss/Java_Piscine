3e8b290d8c43cd19f9869ee16dbbfcb312bb6991
otttisss <otttisss@yandex.ru>
2023-06-22 22:59:35 +0300

    day06 ex02 ProductsRepositoryJdbcImpl completed

--- day06/ex02/Tests/src/main/java/edu/school21/repositories/ProductsRepositoryJdbcImpl.java
+++ day06/ex02/Tests/src/main/java/edu/school21/repositories/ProductsRepositoryJdbcImpl.java
@@ -2,10 +2,7 @@ package edu.school21.repositories;
 
 import edu.school21.models.Product;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
@@ -44,21 +41,67 @@ public class ProductsRepositoryJdbcImpl implements ProductsRepository {
 
     @Override
     public Optional<Product> findById(Long id) throws SQLException {
+        String query = "select * from shop.products where id = ";
+
+        try (Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement()) {
+
+            statement.execute(query + id);
+            ResultSet resultSet = statement.getResultSet();
+
+            if (resultSet.next()) {
+                return Optional.of(new Product(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getLong("price")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return Optional.empty();
     }
 
     @Override
     public void update(Product product) throws SQLException {
+        String query = "update shop.products set name = ?, price = ? where id = ?";
 
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, product.getName());
+            statement.setLong(2, product.getPrice());
+            statement.setLong(3, product.getId());
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void save(Product product) throws SQLException {
+        String query = "insert into shop.products (name, price) values (?, ?)";
 
+        try (Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query)){
+
+            statement.setString(1, product.getName());
+            statement.setLong(2, product.getPrice());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(Long id) throws SQLException {
+        String query = "delete from shop.products where id = ";
 
+        try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+
+            statement.execute(query + id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
