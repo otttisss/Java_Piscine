1cecf93179e62331e9c885a78205086057a9e199
otttisss <otttisss@yandex.ru>
2023-06-22 17:35:52 +0300

    day06 ex00 NumberWorker class completed // TODO: Junit tests

--- day06/ex00/Tests/src/main/java/edu/school21/numbers/NumberWorker.java
+++ day06/ex00/Tests/src/main/java/edu/school21/numbers/NumberWorker.java
@@ -1,4 +1,31 @@
 package edu.school21.numbers;
 
 public class NumberWorker {
+    public boolean isPrime(int number) {
+        if (number < 2)
+            throw new IllegalNumberException("Illegal number!");
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public int digitsSum(int number) {
+        int res = 0;
+
+        if (number < 0)
+            number = -number;
+        while (number != 0) {
+            res += number % 10;
+            number /= 10;
+        }
+        return res;
+    }
+}
+
+class IllegalNumberException extends RuntimeException {
+    public IllegalNumberException(String msg) {
+        super(msg);
+    }
 }
