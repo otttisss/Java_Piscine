f12ee00b0af673539bd2faeb82fceb9aaa5a4d5e
otttisss <otttisss@yandex.ru>
2023-06-22 22:44:01 +0300

    day06 ex02 in progress

--- day06/ex02/Tests/src/main/java/edu/school21/repositories/ProductsRepositoryJdbcImpl.java
+++ day06/ex02/Tests/src/main/java/edu/school21/repositories/ProductsRepositoryJdbcImpl.java
@@ -1,4 +1,64 @@
 package edu.school21.repositories;
 
-public class ProductsRepositoryJdbcImpl {
+import edu.school21.models.Product;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import java.util.LinkedList;
+
+public class ProductsRepositoryJdbcImpl implements ProductsRepository {
+    private DataSource dataSource;
+
+    public ProductsRepositoryJdbcImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public List<Product> findAll() throws SQLException {
+        List<Product> productList = new LinkedList<>();
+        String query = "select * from shop.products";
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.execute(query);
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                productList.add(new Product(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getLong("price")
+                ));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productList;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) throws SQLException {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Product product) throws SQLException {
+
+    }
+
+    @Override
+    public void save(Product product) throws SQLException {
+
+    }
+
+    @Override
+    public void delete(Long id) throws SQLException {
+
+    }
 }
