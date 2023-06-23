296c390d3823f6aa6cf9c6768273c9c844c85976
otttisss <otttisss@yandex.ru>
2023-06-23 16:05:06 +0300

    day07 ex00 Car class created

--- day07/ex00/Reflection/src/main/java/edu/school21/classes/Car.java
+++ day07/ex00/Reflection/src/main/java/edu/school21/classes/Car.java
@@ -1,4 +1,36 @@
 package edu.school21.classes;
 
 public class Car {
+    private Long number;
+    private String model;
+    private Double price;
+    private Boolean reserved;
+
+    public Car() {
+        this.number = 0L;
+        this.model = "Default";
+        this.price = 0.0;
+        this.reserved = false;
+    }
+
+    public Car(Long number, String model, Double price, Boolean reserved) {
+        this.number = number;
+        this.model = model;
+        this.price = price;
+        this.reserved = reserved;
+    }
+
+    public void setNewPrice(Double newPrice) {
+        price = newPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "number=" + number +
+                ", model=" + model + '\'' +
+                ", price=" + price +
+                ", reserved=" + reserved +
+                '}';
+    }
 }
