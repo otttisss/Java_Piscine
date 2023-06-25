0d55d75b8ef07e385a7cf944935443b991fb6ed8
otttisss <otttisss@yandex.ru>
2023-06-25 18:22:42 +0300

    day08 ex01 User and Crud/Users repositories added

--- day08/ex01/Service/src/main/java/school21/spring/service/repositories/CrudRepository.java
+++ day08/ex01/Service/src/main/java/school21/spring/service/repositories/CrudRepository.java
@@ -1,4 +1,11 @@
 package school21.spring.service.repositories;
 
-public class CrudRepository {
+import java.util.List;
+
+public interface CrudRepository<T> {
+    T findById(Long id);
+    List<T> findAll();
+    void save(T entity);
+    void update(T entity);
+    void delete(Long id);
 }
--- day08/ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepository.java
+++ day08/ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepository.java
@@ -1,4 +1,9 @@
 package school21.spring.service.repositories;
 
-public class UsersRepository {
+import school21.spring.service.models.User;
+
+import java.util.Optional;
+
+public interface UsersRepository extends CrudRepository<User> {
+    Optional<User> findByEmail(String email);
 }
