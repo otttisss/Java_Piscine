dcaec5434faa6da6cb3ebdbc06f93e1c3e8b32b3
otttisss <otttisss@yandex.ru>
2023-06-25 17:54:25 +0300

    day08 ex00 completed

--- day08/ex00/Spring/src/main/java/edu/school21/spring/app/Program.java
+++ day08/ex00/Spring/src/main/java/edu/school21/spring/app/Program.java
@@ -2,6 +2,7 @@ package edu.school21.spring.app;
 
 import edu.school21.spring.preprocessor.PreProcessor;
 import edu.school21.spring.preprocessor.PreProcessorToUpperImpl;
+import edu.school21.spring.printer.Printer;
 import edu.school21.spring.printer.PrinterWithPrefixImpl;
 import edu.school21.spring.render.Renderer;
 import edu.school21.spring.render.RendererErrImpl;
@@ -9,14 +10,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Program {
     public static void main(String[] args) {
-        System.out.println("--------------------------");
         PreProcessor preProcessor = new PreProcessorToUpperImpl();
         Renderer renderer = new RendererErrImpl(preProcessor);
         PrinterWithPrefixImpl printer = new PrinterWithPrefixImpl(renderer);
-        printer.setPrefix("Prefix");
+        printer.setPrefix("Prefix ");
         printer.print("Hello!");
-        System.out.println("--------------------------");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        Printer printer1 = context.getBean("printerWithPrefix", Printer.class);
+        printer1.print("Hello!");
     }
 }
